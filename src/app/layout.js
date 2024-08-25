6131a434f4842efa0ba28e1e5d9221fcb06389d8
ram_tanniru <ramtanniru2@gmail.com>
2024-08-25 12:07:13 +0530

    init

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Head>
+          <title>21BCE9789</title> {/* Replace with your roll number */}
+        </Head>
+        {children}
+      </body>
     </html>
   );
 }
